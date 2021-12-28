2469c5cc0148200d1725925c49312946dfedabdc
simon brahan <tntpiranha@gmail.com>
2021-12-28 15:05:27 +0000

    Day 11 factored code into module for use in part 2

--- 11/part1.py
+++ 11/part1.py
@@ -1,32 +1,4 @@
-def get_neighbours(x, y, squids):
-    directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-
-    out = []
-    for add_x, add_y in directions:
-        neighbour_x = x + add_x
-        neighbour_y = y + add_y
-
-        if 0 <= neighbour_y < len(squids) and 0 <= neighbour_x < len(squids[neighbour_y]):
-            out.append((neighbour_x, neighbour_y))
-
-    return out
-
-
-def maybe_flash(x, y, squids, flashed_squids):
-
-    if squids[y][x] <= 9 or (x, y) in flashed_squids:
-        return squids, flashed_squids
-
-    flashed_squids.add((x, y))
-
-    neighbours = get_neighbours(x, y, squids)
-
-    for neighbour_x, neighbour_y in neighbours:
-        squids[neighbour_y][neighbour_x] += 1
-        squids, flashed_squids = maybe_flash(neighbour_x, neighbour_y, squids, flashed_squids)
-
-    return squids, flashed_squids
-
+from squids import maybe_flash
 
 with open('input.txt') as f:
     squids = [[int(level) for level in row.strip()] for row in f]
