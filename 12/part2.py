5b636b33933e9212435cec57e224fb99b59a0d53
simon brahan <tntpiranha@gmail.com>
2022-01-04 13:30:46 +0000

    Day 12 part 2 - Removed extra nesting

--- 12/part2.py
+++ 12/part2.py
@@ -3,7 +3,7 @@ from copy import copy
 
 small_caves = set()
 links = defaultdict(list)
-with open('input.txt') as f:
+with open('sample.txt') as f:
     for line in f:
         left, right = line.strip().split('-')
 
@@ -25,25 +25,25 @@ while len(paths) > 0:
 
     # If we're in the end cave, add this to the complete paths and skip to the next path
     if current_cave == 'end':
+        print(','.join(path))
         complete_paths.append(path)
         continue
 
+    new_cave_visit_count = copy(cave_visit_count)
+    # If this option is a small cave, check whether we're allowed to visit it again
+    if current_cave in small_caves:
+        # If we've visited this cave twice, no go
+        if cave_visit_count[current_cave] == 2:
+            continue
+
+        # If we've visited this cave once and ANY small cave twice, no go
+        if cave_visit_count[current_cave] == 1 and 2 in cave_visit_count.values():
+            continue
+
+        new_cave_visit_count[current_cave] += 1
+
     options = links[current_cave]
     for option in options:
-        new_cave_visit_count = copy(cave_visit_count)
-
-        # If this option is a small cave, check whether we're allowed to visit it again
-        if option in small_caves:
-            # If we've visited this cave twice, no go
-            if cave_visit_count[option] == 2:
-                continue
-
-            # If we've visited this cave once and ANY small cave twice, no go
-            if cave_visit_count[option] == 1 and 2 in cave_visit_count.values():
-                continue
-
-            new_cave_visit_count[option] += 1
-
         paths.append((path + [option], new_cave_visit_count))
 
 print(len(complete_paths))
